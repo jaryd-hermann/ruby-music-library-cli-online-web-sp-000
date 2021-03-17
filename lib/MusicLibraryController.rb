147a6c88207b1d44192f3d3b4cec58aaf94eef97
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-17 21:48:45 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -79,15 +79,15 @@ class MusicLibraryController
     end
   end
 
-    def self.play_song
-      puts "Which song number would you like to play?"
-      input = gets.strip.to_i
-
-      if input > 0 && input <= Song.all.length
+  def self.play_song
+    puts "Which song number would you like to play?"
+    if input > 0 && input <= Song.all.length
       array = Song.all.sort{|a, b| a.name <=> b.name}
       song = array[input-1]
       puts "Playing #{song.name} by #{song.artist.name}"
     end
-end
-end
+  end
+
+
+
 end #end
