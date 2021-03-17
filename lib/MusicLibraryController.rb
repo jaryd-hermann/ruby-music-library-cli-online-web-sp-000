759bc16bf8ec65c85586f959a4dd38952b3289ba
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-17 21:49:53 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -47,6 +47,7 @@ class MusicLibraryController
      Song.all.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
       puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
     end
+  end
 
     def self.list_artists
       Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i|
