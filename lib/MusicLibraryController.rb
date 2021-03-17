3b33610300e742786358be79c8df166e46ce1418
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-17 21:42:10 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -44,11 +44,13 @@ class MusicLibraryController
 
    #CLI Methods
    def self.list_songs
-     Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
-      puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+     Song.all.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
+      puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
     end
 
     def self.list_artists
+      Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i|
+      puts "#{i+1}. #{a.name}"
     end
 
     def self.list_genres
