95ed3a3a85324870f57d9e654de0a663e7eb0253
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-17 22:04:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -63,8 +63,8 @@ def self.new_from_filename(filename)
   end
 
 
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name)
-end
+  def self.find_or_create_by_name(song)
+     find_by_name(song) || create(song)
+   end
 
 end #end class
